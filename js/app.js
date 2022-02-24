7b3e787f323dadc2f2d4f0308c49eb8e9604f571
Luis Sandoval <luismsandoval96@gmail.com>
2022-02-23 18:48:26 -0800

    wrapped first question in a function

--- js/app.js
+++ js/app.js
@@ -1,26 +1,28 @@
 'use strict';
 
-console. log('hello world');
+console.log('hello world');
 
-alert ('Welcome to my About Me page!');
+alert('Welcome to my About Me page!');
 
 let usrnm = prompt('What do you like to be called?');
-console. log(usrnm);
-alert('Cheers ' + usrnm+ '!');
+console.log(usrnm);
+alert('Cheers ' + usrnm + '!');
 
 alert('Guesing game: Please answer the following, yes or no, questions.');
+function havePets() {
+  let pets = prompt('Do you think I have any pets?').toLowerCase();
 
-let pets = prompt ('Do you think I have any pets?').toLowerCase();
-
-if (pets === 'yes' || pets === 'y') {
+  if (pets === 'yes' || pets === 'y') {
   /*console. log*/alert('Not at the moment ' + usrnm + 'but quite a few over my lifetime.');
-} else if (pets === 'no' || pets === 'n') {
+  } else if (pets === 'no' || pets === 'n') {
   /*console. log*/alert('Wow ' + usrnm + '. Spot on guess!');
-} else {
+  } else {
   /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
+  }
 }
+havePets();
 
-let snow = prompt ('Do I like the snow?').toLowerCase();
+let snow = prompt('Do I like the snow?').toLowerCase();
 
 if (snow === 'yes' || snow === 'y') {
   /*console. log*/alert('I take rain over snow; any day of the week.');
@@ -30,7 +32,7 @@ if (snow === 'yes' || snow === 'y') {
   /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
 }
 
-let champ = prompt ('Is Boston the championship city?').toLowerCase();
+let champ = prompt('Is Boston the championship city?').toLowerCase();
 
 if (champ === 'yes' || champ === 'y') {
   /*console. log*/alert('Of course! Evey Boston spots team has current championships.');
@@ -41,17 +43,17 @@ else if (champ === 'no' || champ === 'n') {
   /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
 }
 
-let bruno = prompt ('Is my middle name Bruno?').toLowerCase();
+let bruno = prompt('Is my middle name Bruno?').toLowerCase();
 
 if (bruno === 'yes' || bruno === 'y') {
   /*console. log*/alert('No; and nobody is suppose to talk about Bruno.');
-} else if ( bruno === 'no' || bruno === 'n') {
+} else if (bruno === 'no' || bruno === 'n') {
   /*console. log*/alert('Correct! Now off the subject of Bruno; why did you bring it up?!');
 } else {
   /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
 }
 
-let bean = prompt ('Is the city of Boston known for beans?').toLowerCase();
+let bean = prompt('Is the city of Boston known for beans?').toLowerCase();
 
 if (bean === 'yes' || bean === 'y') {
   /*console. log*/alert('Correct; and locally the city is reffered to as The Bean or Bean Town!');
