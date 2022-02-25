77b39ef22f91c0a6211927896a049dfbf516af63
Jeramie Green <93801788+JeramieGreen@users.noreply.github.com>
2022-02-24 21:32:49 -0800

    updates to questions from lab-03, added comments, moved func calling to bottom of js

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 ## About Me
 
-### Author: Jeramie Green
+### Author: Jeramie Green, Luis S
 
 ### Links and Resources
 
 ### Reflections and Comments
 
+- I find that the repitition is key; for getting things committed to memory. One other thing I found was that if I am running into an issue, it is usually something simple that is easily overlooked. A new set of eyes can save a lot of time in those cases.
+
+- Early on in this process I found that my curve for getting back into the workflow of codefellows and development was a little steaper than I had expected. Working with some of the other students has brought down that curve in an amazing way.
+
 - Luis S was the driver while I was the navigator. We wrapped the logic of the questions into functions, and ACP'd after every question.
+
+- Today I am updating my lab-03. I finished my questions. I found them to be easier than I thought they would be to finish.
--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ alert('Cheers ' + usrnm + '!');
 
 alert('Guesing game: Please answer the following, yes or no, questions.');
 
+let score = 0;
+
+//Question One
 function havePets() {
   let pets = prompt('Do you think I have any pets?').toLowerCase();
 
@@ -17,12 +20,13 @@ function havePets() {
   /*console. log*/alert('Not at the moment ' + usrnm + 'but quite a few over my lifetime.');
   } else if (pets === 'no' || pets === 'n') {
   /*console. log*/alert('Wow ' + usrnm + '. Spot on guess!');
-  } else {
+    score++;
+  }else {
   /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
   }
 }
-havePets();
 
+//Question Two
 function weather() {
   let snow = prompt('Do I like the snow?').toLowerCase();
 
@@ -30,26 +34,27 @@ function weather() {
   /*console. log*/alert('I take rain over snow; any day of the week.');
   } else if (snow === 'no' || snow === 'n') {
   /*console. log*/alert('Correct! We never had snow days, but always had snow growing up. Ha');
-  } else {
+    score++;
+  }else {
   /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
   }
 }
-weather();
 
+//Question Three
 function champCity() {
   let champ = prompt('Is Boston the championship city?').toLowerCase();
 
   if (champ === 'yes' || champ === 'y') {
   /*console. log*/alert('Of course! Evey Boston spots team has current championships.');
-  }
-  else if (champ === 'no' || champ === 'n') {
+    score++;
+  }else if (champ === 'no' || champ === 'n') {
   /*console. log*/alert('I would feel that way if I was coming from your town too.');
   } else {
   /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
   }
 }
-champCity();
 
+//Question Four
 function middleName() {
   let bruno = prompt('Is my middle name Bruno?').toLowerCase();
 
@@ -57,21 +62,78 @@ function middleName() {
   /*console. log*/alert('No; and nobody is suppose to talk about Bruno.');
   } else if (bruno === 'no' || bruno === 'n') {
   /*console. log*/alert('Correct! Now off the subject of Bruno; why did you bring it up?!');
-  } else {
+    score++;
+  }else {
   /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
   }
 }
-middleName();
 
+//Question Five
 function cityFact() {
   let bean = prompt('Is the city of Boston known for beans?').toLowerCase();
 
   if (bean === 'yes' || bean === 'y') {
   /*console. log*/alert('Correct; and locally the city is reffered to as The Bean or Bean Town!');
+    score++;
   } else if (bean === 'no' || bean === 'n') {
   /*console. log*/alert('Wow. You never heard of Boston Baked Beans!!');
   } else {
-  /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
+    /*console. log*/alert('Please answer with yes or no only. Thanks ' + usrnm + '!');
   }
 }
-cityFact();
+
+//Question Six
+function countries() {
+  let shotsLeft = 3;
+  for (let i = 0; i < 4; i++) {
+    let number = parseInt(prompt('From 1 - 10; in how many countries have I lived?'));
+    if (number === 5) {
+      /*console. log*/alert('Including the US, yes!');
+      score++;
+      break; 
+    } if (number < 5) {
+    /*console. log*/alert(`Just a little higher; you have ${shotsLeft--} shots left!`);
+    } if (number > 5) {
+    /*console. log*/alert(`One day! But try a lower number. You still have ${shotsLeft--} shots left.`);
+    }
+  } /*console. log*/alert('5 countries lived so far.');
+}
+
+//Question Seven
+function turtle(){
+//            0.              1.          2.        .3          4.          5.
+  let tmnt = ['michelangelo','donatello','raphael','leonardo','splinter','metalhead'];
+
+  console. log(tmnt.length);
+
+  let moreAttempts = 6;
+  let rightAnswer = false;
+
+  for (let i = 0; i < 6; i++) {
+    let userResponse = prompt(`Name a Ninja Turtle character; you have ${moreAttempts-i} chances left!`).toLowerCase();
+    for (let j = 0; j < tmnt.length; j++) {
+      if (userResponse === tmnt[j]) {
+        console. log('That is correct!');
+        rightAnswer = true;
+        score++;
+        break;
+      } if (userResponse !== tmnt[j]) {
+        console. log('Incorrect, this time.');
+        break;
+      }
+    } if (rightAnswer) {
+      break;
+    }
+  }
+}
+
+// This is where my functions are being called
+/*question one*/havePets();
+/*question two*/weather();
+/*question three*/champCity();
+/*question four*/middleName();
+/*question five*/cityFact();
+/*question six*/countries();
+/*question seven*/turtle();
+
+alert('Thanks for playing ' + usrnm + '! You scored a ' + score + ' out of 7!');
